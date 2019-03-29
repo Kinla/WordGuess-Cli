4d23b70e8f18fbb054602de61e23f89da2e827a0
SharonC <sharon.s.c.chien@gmail.com>
2019-03-29 02:20:39 -0400

    letter seems to work. working on last function in word.js

---
+++ word.js
@@ -0,0 +1,37 @@
+/*
+
+* **Word.js**: Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess. That means the constructor should define:
+
+  * An array of `new` Letter objects representing the letters of the underlying word
+
+  * A function that returns a string representing the word. This should call the function on each letter object (the first function defined in `Letter.js`) that displays the character or an underscore and concatenate those together.
+
+  * A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in `Letter.js`)
+
+
+*/
+
+let Letter = require("./Letter")
+
+class Word {
+    constructor (input) {
+        this.letters = input.split("")
+        this.array = this.letters.map(el => new Letter(el))
+    }
+    toString(){
+        this.string = this.array.map(ltr => ltr.display()).join(" ")
+        return this.string
+    }
+    match(char){
+        this.array.forEach(ltr => {
+            check(char)
+        });
+    }
+}
+
+let bench = new Word("bench")
+
+console.log(bench.toString())
+
+bench.match("a")
+
